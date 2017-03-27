63ed07d1b2f69ce80f90c7b266f05cb0f6d6d56a
Samuel Harrold <samuel.harrold@gmail.com>
2017-03-27 19:06:46 -0400

    app_api: started

---
+++ demo/app_api/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""Demo application with an API.
+
+"""
+
+
+# Import standard packages.
+# Import installed packages.
+# Import local packages.
+from . import main
+from . import api
+
+
+# Define package index.
+__all__ = [
+    'main',
+    'api']
